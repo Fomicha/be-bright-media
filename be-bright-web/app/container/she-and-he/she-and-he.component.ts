7938edfa6cfbfdf6c48997b75814998d341045c9
Fomicha <fomich.aleksandr@gmail.com>
2020-12-17 14:22:50 +0200

    update angular

---
+++ be-bright-web/app/container/she-and-he/she-and-he.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bb-she-and-he',
+  templateUrl: './she-and-he.component.html',
+  styleUrls: ['./she-and-he.component.scss']
+})
+export class SheAndHeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
